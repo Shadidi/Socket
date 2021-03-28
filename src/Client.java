231c007d81dda3ed6373ffec859be16a1e13a692
Shadidi <66976280+Shadidi@users.noreply.github.com>
2021-03-28 18:42:45 -0500

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -13,25 +13,28 @@ public class Client{
 	private Socket socket = null;
 	private DataInputStream input = null;
 	private DataOutputStream out = null;
+	private String username = "";
 	
 	
 	public Client(String address, int port) {
 		
-		String username = "[" + address + "]";
 		try {
 			
 			System.out.println("Enter your name: ");	//if they enter nothing they get represented by their IP
-
+			username = "[" + address + "]";
 			input = new DataInputStream(System.in); // whatever the client is typing
-			if (input.readLine() == "") {
-				username = "[" + input.readLine() + "]";
+
+			String name = input.readLine();
+			if (!name.equals("")) {
+				username = "[" + name + "]";
 			}
-			
+			//InetAddress ip = InetAddress.getByName("localhost"); gets your IP address if you want to use that later
 			socket = new Socket (address, port); //ask name first, THEN connect so the first line said isn't the username
 			
 			System.out.println("Connected");
 			
 			out = new DataOutputStream(socket.getOutputStream()); // sends out client's input
+			out.writeUTF("<<" + username + " HAS JOINED THE CHAT>>"); //This is sent to nobody, but server needs a message before it accepts more clients
 		
 		}
 			
@@ -43,9 +46,10 @@ public class Client{
 		}
 		
 		String line = "";
+		//send.start();  //The main issue: Only the 1st message sends. Based on debugging println the issue is in client. Server keeps listening it seems
 		
 		
-		while (!line.equals(Server.exitWord)) {
+		/*while (!line.equals(Server.exitWord)) {
 			
 			try {
 				
@@ -69,8 +73,57 @@ public class Client{
 			System.out.println(i);
 		}
 		
-	}
+	}*/ //original code, just in case
 	
+		Thread read = new Thread(new Runnable()  { 
+            @Override
+            public void run() { 
+            	String line = "";
+                while (true) { 
+                    try { 
+                        // read the message sent to this client 
+                        line = input.readUTF(); 
+                        System.out.println(line); 
+                    } 
+                    catch (IOException e) { 
+  
+                        e.printStackTrace(); 
+                    } 
+                } 
+            } 
+		});
+		
+		Thread send = new Thread(new Runnable()  
+        { 
+            @Override
+            public void run() { 
+            	String line = "";
+            	while (!line.equals(Server.exitWord)) {
+        			try {
+        				line = input.readLine();
+        				out.writeUTF(username); //This format is because because Server checks the last UTF message sent and flips out if it sees a name
+        				out.writeUTF(line);
+        			}
+        			catch(IOException i) {
+        				System.out.println(i);
+        			}
+        		}
+        		
+        		try {
+        			input.close();
+        			out.close();
+        			socket.close();
+        		}
+        		catch(IOException i){
+        			System.out.println(i);
+        		}
+            } 
+        }); 
+		
+		send.start();
+		read.start();
+	}
+		
 	public static void main(String args[]) {
         // start the client
 		// Client client2 = new Client 
